7acbaaaa09db1914d2f9a78c55aeb6795db945a4
Desiree_McElroy <desireeashley90@gmail.com>
2021-06-02 21:25:14 -0700

    adding exercises

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -24,3 +24,28 @@ and countrycode like 'usa'
 select *
 from city
 where id = '1661'
+
+
+-- Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+select *
+from city
+where countrycode = 'JPN';
+
+
+-- Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+select name
+from city
+where countrycode like 'JPN';
+
+-- Query a list of CITY and STATE from the STATION table.
+select city, state
+from station;
+
+-- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+select distinct(city)
+from station
+where mod(id,2)=0;
+
+-- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+select count (city) - count(distinct city) 
+from station;
