9d53593290c6959a97523580406ddc2d95d9f748
Desiree_McElroy <desireeashley90@gmail.com>
2021-07-18 18:54:14 -0500

    adding more exercises

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -49,3 +49,19 @@ where mod(id,2)=0;
 -- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 select count (city) - count(distinct city) 
 from station;
+
+-- Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
+-- If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+select city, length(city) 
+from station
+order by length(city) desc, city
+limit 1;
+select city, length(city) 
+from station
+order by length(city) asc, city 
+limit 1;
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+select distinct city
+from station
+where city regexp "^[aeiou].*";
