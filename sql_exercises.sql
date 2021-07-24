7b3ba2dba4b7c8a063ba608877c2c2f3f70bfddf
Desiree_McElroy <desireeashley90@gmail.com>
2021-07-24 10:36:30 -0500

    adding more exercises for aggregates

--- sql_exercises.sql
+++ sql_exercises.sql
@@ -65,3 +65,13 @@ limit 1;
 select distinct city
 from station
 where city regexp "^[aeiou].*";
+
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+select distinct city
+from station
+where city like '%a'
+or city like '%e'
+or city like '%i'
+or city like '%o'
+or city like '%u';
+
